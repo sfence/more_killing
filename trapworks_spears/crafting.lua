434510af3d7ca0bec0ebb018c1fcb6a1c96edbaf
SFENCE <sfence.software@gmail.com>
2025-10-15 19:49:04 +0200

    Update list of valdi twigs.

--- trapworks_spears/crafting.lua
+++ trapworks_spears/crafting.lua
@@ -19,12 +19,18 @@ core.register_craft({
 
 local inv_next_line_offset = moretools.adaptation.player_mod.next_line_offset_inv
 
+local twigs_to_support = {
+	["trunks:twig_2"] = true,
+	["trunks:twig_3"] = true,
+	["trunks:twig_10"] = true,
+}
+
 table.insert(moretools.knife_actions, {
 	type = "node",
 	action_on_use = function(action, user, pointed_thing)
 		local node = core.get_node(pointed_thing.under)
 		local inv = user:get_inventory()
-		if (node.name == "trunks:twig_2") or (node.name == "trunks:twig_3") then
+		if twigs_to_support[node.name] then
 			local new_item = ItemStack("trapworks_spears:spear_support")
 			if inv:room_for_item("main", new_item) then
 				inv:add_item("main", new_item)
